248ce138748680b4374c6cc971bb3dedfb12b52d
Anthony Durity <github@jollyrotten.org>
2011-09-18 00:46:26 +0300

    ESC gets rid of menu

--- js/file-folder-utilities-0.1.js
+++ js/file-folder-utilities-0.1.js
@@ -1,16 +1,5 @@
 /* File & Folder utilities */
 
-function displayMenu(target_obj)
-{
-	console.log(target_obj);
-	$("#file-folder").position({
-	  my: "left top",
-	  at: "right bottom",
-	  of: target_obj
-	});
-	$("#file-folder").show();
-}
-
 var once = false;
 
 /*
@@ -22,12 +11,15 @@ var once = false;
  * $(selector).mouseenter(handlerIn).mouseleave(handlerOut);
  */
  
+var keep_track = 0;
+ 
 var superMenuIn = function(ev) {
   
   // get the position and width an height of the placeholder element
-  var target = "#"+ev.currentTarget.id;
-  document.superFolder.EpageID.value = ev.currentTarget.id.split('-')[2];
-  document.superPage.EpageID.value = ev.currentTarget.id.split('-')[2];
+  var curr_id = ev.currentTarget.id;
+  var target = "#"+curr_id;
+  document.superFolder.EpageID.value = curr_id.split('-')[2];
+  document.superPage.EpageID.value = curr_id.split('-')[2];
   var pos = $(target).offset();  
   var width = $(target).width();
   var height = $(target).height();
@@ -41,24 +33,36 @@ var superMenuIn = function(ev) {
   /* alert ("target " + target + " t pos.left " + pos.left + " t pos.top " + pos.top + " t width " + width + " ff width " + ff_width); */
   
   // show the menu to the left and below the placeholder
-  $("#file-folder").css( { "left": ((pos.left - ff_width) + "px"), "top": ((pos.top + height) + "px") } );
+  $("#file-folder").css( { "left": ((pos.left - ff_width -8) + "px"), "top": ((pos.top + height) + "px") } );
   
   // for debugging purposes, will cease to be
-  $("#custom-stuff").empty();
-  $("#custom-stuff").append(ev.currentTarget.id);
+  // $("#custom-stuff").empty();
+  // $("#custom-stuff").append(curr_id);
+  
+  $("#file-folder").show();
+  document.superFolder.NewObject.focus();
   
   // 
   $("#file-folder").keyup(function(eventage) {
   	/* alert(eventage.which + " ... " + eventage.keyCode); */
   	// fix toggle glitch!!!
-	if (eventage.keyCode == 27) { superMenuOut(); } 
-});
-  $("#file-folder").show();
-   document.superFolder.NewObject.focus();
-  /* console.log(ev);
-  console.log(ev.currentTarget);
-  console.log(ev.currentTarget.id);
-  console.log(target); */
+	if (eventage.keyCode == 27) {
+		console.log(eventage);
+		eventage.stopImmediatePropagation();
+		console.log(target);
+		$(target).trigger('click');
+		$(target).unbind();
+		$(target).toggle(superMenuIn, superMenuOut);
+		superMenuOut(); // just in case :/
+	} 
+  });
+  
+  /*
+  window.console && console.log && console.log(ev);
+  window.console && console.log && console.log(ev.currentTarget);
+  window.console && console.log && console.log(ev.currentTarget.id);
+  window.console && console.log && console.log(target);
+  */
 }
 
 var superMenuOut = function(ev) {
